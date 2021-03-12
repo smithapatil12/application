a3e8498e4ebca1e17b1a4637781f6203385a25bb
smithapatil12 <77383320+smithapatil12@users.noreply.github.com>
2021-03-12 11:11:07 +0530

    Add files via upload

---
+++ src/main/java/com/example/logindemo/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.logindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+ private EditText Name;
+ private EditText Password;
+ private TextView Info;
+ private Button Login;
+ private int counter=5;
+ private TextView userREgistration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name =(EditText) findViewById(R.id.etName);
+        Password=(EditText)findViewById(R.id.etPassword);
+        Login=(Button)findViewById(R.id.btnLogin);
+        userREgistration=(TextView)findViewById(R.id.tvRegister) ;
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(),Password.getText().toString());
+            }
+        });
+        userREgistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,RegistrationActivity.class));
+            }
+        });
+
+    }
+    private void validate (String userName,String userPassword){
+        if( (userName.equals("smitha")) && (userPassword.equals("1234")) ){
+            Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+            startActivity(intent);
+        }else{
+            counter--;
+            if (counter == 0){
+                Login.setEnabled(false);
+            }
+        }
+
+    }
+}
